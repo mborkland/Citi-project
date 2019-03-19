8af48b83f54e1e4b91e63f3251bfd81d82e324bd
mborkland <mike.borkland@gmail.com>
2019-03-19 14:53:49 -0400

    started login page and UI

--- src/main/java/edu/usf/cse/controller/MainController.java
+++ src/main/java/edu/usf/cse/controller/MainController.java
@@ -7,6 +7,7 @@ import edu.usf.cse.service.TransactionRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
