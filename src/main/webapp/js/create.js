18fbbfc20677ef552dde165511c7ac9e6bd42d7c
sbhat68 <sbhat4@mail.usf.edu>
2019-03-26 16:55:08 -0400

    Modified some fields in the create transaction record

--- src/main/webapp/js/create.js
+++ src/main/webapp/js/create.js
@@ -35,7 +35,7 @@ app.controller('CreateController', ['$scope', '$http', '$state', function($scope
         url += 'requestor=' + $scope.txData.requestor;
         $http.post(url).then(function (response) {
             console.log(response);
-            alert('Customer screening record created successfully');
+            alert('Transaction screening record created successfully');
             $state.go('read');
         }, function (error) {
             console.log(error);
